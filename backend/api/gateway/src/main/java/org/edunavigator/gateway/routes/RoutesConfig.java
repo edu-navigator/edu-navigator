afcd4e99c53c7e4bd89fc430f3459b47f153d583
Nonelela4980 <nonelelacele@gmail.com>
2024-09-02 09:26:21 +0200

    Created an API Gateway

---
+++ backend/api/gateway/src/main/java/org/edunavigator/gateway/routes/RoutesConfig.java
@@ -0,0 +1,44 @@
+package org.edunavigator.gateway.routes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+@Configuration
+public class RoutesConfig {
+    //urls
+    @Value("${services.ingestion.host}")
+    private String ingestionServiceHost;
+    @Value("${services.search.host}")
+    private String searchServiceHost;
+    @Value("${services.content.host}")
+    private String contentServiceHost;
+    //paths
+    @Value("${services.ingestion.path}")
+    private String ingestionPath;
+    @Value("${services.search.path}")
+    private String searchPath;
+    @Value("${services.content.path}")
+    private String contentPath;
+    @Bean
+    public RouterFunction<ServerResponse> ingestionServiceRoute(){
+        return GatewayRouterFunctions.route("ingestion_service")
+                .route(RequestPredicates.path(ingestionPath), HandlerFunctions.http(ingestionServiceHost))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> searchServiceRoute(){
+        return GatewayRouterFunctions.route("search_service")
+                .route(RequestPredicates.path(searchPath), HandlerFunctions.http(searchServiceHost))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> contentServiceRoute(){
+        return GatewayRouterFunctions.route("content_service")
+                .route(RequestPredicates.path(contentPath), HandlerFunctions.http(contentServiceHost))
+                .build();
+    }
+}
